74dd8938942ff97a3f503e433930e2da11d43398
Nicolas Joubert <njoubert@clever-age.com>
2024-10-28 14:27:47 +0100

    #1 Add Makefile & .docker for local standalone usage. Add rector, phpstan & php-cs-fixer configurations. Remove phpcs configuration.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,31 +1,46 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/src')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true)
-    ->exclude(['build', 'vendor'])
-    ->files()
-    ->name('*.php')
-;
+/*
+ * This file is part of the CleverAge/UiProcessBundle package.
+ *
+ * Copyright (c) Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-$config = new PhpCsFixer\Config();
+if (!file_exists(__DIR__.'/src')) {
+    exit(0);
+}
 
-return $config
-    ->setUsingCache(true)
-    ->setRiskyAllowed(true)
-    ->setFinder($finder)
+$fileHeaderComment = <<<'EOF'
+    This file is part of the CleverAge/UiProcessBundle package.
+
+    Copyright (c) Clever-Age
+
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
+    EOF;
+
+return (new PhpCsFixer\Config())
     ->setRules([
-        '@PHP80Migration' => true,
-        '@PHP80Migration:risky' => true,
+        '@PHP71Migration' => true,
+        '@PHP82Migration' => true,
+        '@PHPUnit75Migration:risky' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'fopen_flags' => false,
-        'ordered_imports' => true,
         'protected_to_private' => false,
-        'single_line_throw' => false,
-        // this must be disabled because the output of some tests include NBSP characters
-        'non_printable_character' => false,
+        'native_constant_invocation' => ['strict' => false],
+        'header_comment' => ['header' => $fileHeaderComment],
+        'modernize_strpos' => true,
+        'get_class_to_class_keyword' => true,
     ])
-    ;
+    ->setRiskyAllowed(true)
+    ->setFinder(
+        (new PhpCsFixer\Finder())
+            ->in(__DIR__.'/src')
+            ->in(__DIR__.'/tests')
+            ->append([__FILE__])
+    )
+    ->setCacheFile('.php-cs-fixer.cache')
+;
