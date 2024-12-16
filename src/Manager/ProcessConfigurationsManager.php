06b69a7c8b046aa51b68ab69afb498b4c7b30f95
Tonon Grégory <gtonon@clever-age.com>
2024-12-16 14:18:42 +0100

    #14 Quality. Phpstan use phpstan type to prevent duplicate definitions.

--- src/Manager/ProcessConfigurationsManager.php
+++ src/Manager/ProcessConfigurationsManager.php
@@ -20,6 +20,16 @@ use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * @phpstan-type UiOptions array{
+ *      'source': ?string,
+ *      'target': ?string,
+ *      'entrypoint_type': string,
+ *      'constraints': Constraint[],
+ *      'run': 'null|bool',
+ *      'default': array{'input': mixed, 'context': array{array{'key': 'int|text', 'value':'int|text'}}}
+ *  }
+ */
 final readonly class ProcessConfigurationsManager
 {
     public function __construct(private ProcessConfigurationRegistry $registry)
@@ -39,14 +49,7 @@ final readonly class ProcessConfigurationsManager
     }
 
     /**
-     * @return null|array{
-     *       'source': ?string,
-     *       'target': ?string,
-     *       'entrypoint_type': string,
-     *       'constraints': Constraint[],
-     *       'run': 'null|bool',
-     *       'default': array{'input': mixed, 'context': array{array{'key': 'int|text', 'value':'int|text'}}}
-     * }
+     * @return null|UiOptions
      */
     public function getUiOptions(string $processCode): ?array
     {
@@ -62,16 +65,7 @@ final readonly class ProcessConfigurationsManager
     /**
      * @param array<int|string, mixed> $options
      *
-     * @return array{
-     *     'ui': array{
-     *        'source': ?string,
-     *        'target': ?string,
-     *        'entrypoint_type': string,
-     *        'constraints': Constraint[],
-     *        'run': 'null|bool',
-     *        'default': array{'input': mixed, 'context': array{array{'key': 'int|text', 'value':'int|text'}}}
-     *      }
-     * }
+     * @return array{'ui': UiOptions}
      */
     private function resolveUiOptions(array $options): array
     {
@@ -103,16 +97,7 @@ final readonly class ProcessConfigurationsManager
             $uiResolver->setNormalizer('constraints', fn (Options $options, array $values): array => (new ConstraintLoader())->buildConstraints($values));
         });
         /**
-         * @var array{
-         *      'ui': array{
-         *         'source': ?string,
-         *         'target': ?string,
-         *         'entrypoint_type': 'text|file',
-         *         'constraints': Constraint[],
-         *         'run': 'null|bool',
-         *         'default': array{'input': mixed, 'context': array{array{'key': 'int|text', 'value':'int|text'}}}
-         *       }
-         *  } $options
+         * @var array{'ui': UiOptions} $options
          */
         $options = $resolver->resolve($options);
 
