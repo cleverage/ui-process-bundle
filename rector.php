0e980dc52f8e0082c9fa89ff2d6f09fe71581c20
Nicolas Joubert <njoubert@clever-age.com>
2025-08-11 15:27:00 +0200

    #60 Update rector configuration with minimal version PHP 8.1

--- rector.php
+++ rector.php
@@ -3,31 +3,21 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withPhpVersion(PhpVersion::PHP_84)
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
-    ->withPhpSets(php82: true)
+    ->withPhpSets(php81: true)
     // here we can define, what prepared sets of rules will be applied
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true
-    )
+    ->withComposerBased(doctrine: true, symfony: true)
+    ->withPreparedSets(deadCode: true, codeQuality: true, doctrineCodeQuality: true, symfonyCodeQuality: true)
+    ->withAttributesSets(symfony: true, doctrine: true)
     ->withSets([
-        LevelSetList::UP_TO_PHP_82,
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_71,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        LevelSetList::UP_TO_PHP_81,
     ])
 ;
