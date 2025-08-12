83fe9141d39c4cd8ba6f9dbfdbde86a2e3581825
Nicolas Joubert <njoubert@clever-age.com>
2025-08-12 09:43:22 +0200

    #60 Fix rector configuration because withComposerBased(symfony: true) doesn't work well with ^6.4|^7.3

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -14,10 +15,13 @@ return RectorConfig::configure()
     ])
     ->withPhpSets(php81: true)
     // here we can define, what prepared sets of rules will be applied
-    ->withComposerBased(doctrine: true, symfony: true)
+    ->withComposerBased(doctrine: true)
     ->withPreparedSets(deadCode: true, codeQuality: true, doctrineCodeQuality: true, symfonyCodeQuality: true)
     ->withAttributesSets(symfony: true, doctrine: true)
     ->withSets([
         LevelSetList::UP_TO_PHP_81,
+        SymfonySetList::SYMFONY_64,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ])
 ;
