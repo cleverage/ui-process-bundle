94306d3364ed400a765a9196e549a6ac25c838b5
Nicolas Joubert <njoubert@clever-age.com>
2026-07-21 09:28:32 +0200

    Fix quality

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -33,7 +33,6 @@ use Symfony\Component\PasswordHasher\Hasher\Pbkdf2PasswordHasher;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 /**
- *
  * @extends AbstractCrudController<User>
  */
 #[IsGranted('ROLE_USER')]
